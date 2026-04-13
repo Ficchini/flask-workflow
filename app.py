e3ee5ca3e3af336cee3e0a02bbc6bcf6c0477077
JFicchini <jficchini.dev@gmail.com>
2026-04-13 09:19:03 -0300

    Criação da Rota Sobre

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"mensagem": "Biblioteca Online.\nSeja bem-vindo!"}
+    return {"Biblioteca Online.\nSeja bem-vindo!"}
+
+@app.route("/sobre")
+def status():
+    return {"Somos uma Biblioteca online!\nFaça seu cadastro e começe agora mesmo a ler livros!"}
 
 if __name__ == "__main__":
     app.run(debug=True)
